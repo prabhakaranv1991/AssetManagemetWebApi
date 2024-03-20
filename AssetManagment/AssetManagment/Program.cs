7ee6a5ce92e9b37664c4ac139287429c485f044f
Prabhakaran <46454107+prabhakaranv1991@users.noreply.github.com>
2024-03-20 08:56:18 +0530

    -aaded summary about cors

--- AssetManagment/AssetManagment/Program.cs
+++ AssetManagment/AssetManagment/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Adding cors to avoid localhost call
+// angular run in localhost and API run in localhost. since it will not allowed to call localhost again. So need to allow cors
 builder.Services.AddCors(
     option =>
     {
@@ -31,7 +33,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+//use cors here
 app.UseCors();
 
 app.UseHttpsRedirection();
